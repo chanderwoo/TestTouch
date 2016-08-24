f8f22ffdc504ae5b066ecf5534e0bb5e5c248df3
wdh <15116907853@163.com>
2016-08-24 18:11:23 +0800

    完成缩放，开始写拖拽

--- app/src/main/java/test/wdh/cn/testtouch/customizedview/GestureImageView.java
+++ app/src/main/java/test/wdh/cn/testtouch/customizedview/GestureImageView.java
@@ -30,8 +30,10 @@ public class GestureImageView extends ImageView implements ViewTreeObserver.OnGl
     private float MIN_SCALE;
     private Matrix mMatrix = new Matrix();
     private float[] mMatrixValues = new float[9];
+    private PointF scalePointF;
     private PointF dragPointF;
     private float preDistance = 0;
+    private float[] initMatrixValues = new float[9];
 
     public GestureImageView(Context context) {
         super(context);
@@ -78,38 +80,48 @@ public class GestureImageView extends ImageView implements ViewTreeObserver.OnGl
         int action = event.getAction();
         switch (action) {
             case MotionEvent.ACTION_DOWN:
-                dragPointF = null;
+                scalePointF = null;
                 preDistance = 0;
                 break;
             case MotionEvent.ACTION_MOVE:
                 if (event.getPointerCount() == 2) {//两点触摸事件处理
+                    dragPointF = null;
                     float fX = event.getX(0);
                     float fY = event.getY(0);
                     float sX = event.getX(1);
                     float sY = event.getY(1);
                     float distance = (float) Math.sqrt((Math.pow(fX - sX, 2) + Math.pow(fY - sY, 2)));
-                    Log.i(TAG, "currentScale = " + currentScale);
-                    Log.i(TAG, "distance = " + distance);
-                    if (dragPointF == null) {
-                        dragPointF = new PointF((fX + sX) / 2.0f, (fY + sY) / 2.0f);
+                    if (scalePointF == null) {
+                        scalePointF = new PointF((fX + sX) / 2.0f, (fY + sY) / 2.0f);
                         preDistance = distance;
                     } else {
                         float scale = (distance - preDistance) / mDistance * 8;
                         float judge = currentScale + scale;
-                        scale = judge > MAX_SCALE || judge < MIN_SCALE ? 0 : scale;
+                        scale = judge > MAX_SCALE || judge < MIN_SCALE ?
+                                (judge > MAX_SCALE ? MAX_SCALE - currentScale : MIN_SCALE - currentScale) :
+                                scale;
                         currentScale += scale;
-                        Log.i(TAG, "current = " + currentScale);
-                        scale(currentScale/(currentScale-scale), dragPointF);
+                        scale(currentScale / (currentScale - scale), scalePointF);
                     }
                     preDistance = distance;
                 }
-                if (event.getPointerCount() == 1) {
-                    dragPointF = null;
+                if (event.getPointerCount() == 1) { //处理移动事件
+                    Log.i(TAG, "处理移动事件");
+                    scalePointF = null;
+                    if (dragPointF == null) {
+                        dragPointF = new PointF(event.getX(), event.getY());
+                    } else {
+                        float dragX = event.getX() - dragPointF.x;
+                        float dragY = event.getY() - dragPointF.y;
+                        dragPointF = new PointF(event.getX(), event.getY());
+                        Log.i(TAG, "dragX = " + dragX + "  dragY = " + dragY);
+                        drag(dragX, dragY);
+                    }
                 }
                 break;
             case MotionEvent.ACTION_POINTER_UP:
                 if (event.getPointerCount() == 1) {
-                    dragPointF = null;
+                    scalePointF = null;
                     preDistance = 0;
                 }
                 break;
@@ -122,6 +134,18 @@ public class GestureImageView extends ImageView implements ViewTreeObserver.OnGl
     }
 
     /**
+     * 拖拽imageView
+     *
+     * @param dragX X轴的移动距离
+     * @param dragY Y轴的移动距离
+     */
+    private void drag(float dragX, float dragY) {
+        mMatrix.postTranslate(dragX, dragY);
+        setImageMatrix(mMatrix);
+        mMatrix.setValues(mMatrixValues);
+    }
+
+    /**
      * 按照缩放比例和点进行缩放
      *
      * @param scale 缩放比例
@@ -130,6 +154,10 @@ public class GestureImageView extends ImageView implements ViewTreeObserver.OnGl
     private void scale(float scale, PointF f) {
         mMatrix.postScale(scale, scale, f.x, f.y);
         mMatrix.getValues(mMatrixValues);
+        if (currentScale == MIN_SCALE) {//缩放到初始位置，将图片设置为初始位置,此处可以添加动画，请随意发挥。
+            mMatrix.setValues(initMatrixValues);
+            setImageMatrix(mMatrix);
+        }
         setImageMatrix(mMatrix);
     }
 
@@ -144,7 +172,7 @@ public class GestureImageView extends ImageView implements ViewTreeObserver.OnGl
             int width = d.getIntrinsicWidth();
             int height = d.getIntrinsicHeight();
             float scale = 1.0f;
-            // 有3种情况，1、宽大于屏幕宽。2、高大于屏幕高。3、宽和高均大于屏幕
+            // 有3种情况，1、宽大于屏幕宽。2、高大于屏幕高。3、宽和高均大于屏幕，图像宽高均小于设备的暂时不考虑
             if (width > mWidth && height < mHeight) {
                 scale = mWidth * 1.0f / width;
             }
@@ -160,6 +188,7 @@ public class GestureImageView extends ImageView implements ViewTreeObserver.OnGl
 
             mMatrix.postScale(currentScale, currentScale, mWidth / 2.0f, mHeight / 2.0f);
             mMatrix.getValues(mMatrixValues);
+            mMatrix.getValues(initMatrixValues);
             setImageMatrix(mMatrix);
         }
 
